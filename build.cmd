b50e941f0e0a70ebb296b352c889896ac80473bc
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-06-20 01:06:18 +0200

    build.cmd: does not depend on MERCURY_* environment variables anylonger

--- build.cmd
+++ build.cmd
@@ -1,12 +1,32 @@
+@setlocal enabledelayedexpansion
+@if defined MERCURY_HOME (
+   call :SET_HOME MMC
+) else (
+   call :FIND_IN_PATH mercury.bat MMC
+)
+
+@if defined MMC goto :MAKE
+@echo Cannot find Mercury compiler executable, MERCURY_HOME=%MERCURY_HOME%
+@exit /b 1
+
+:MAKE
 @pushd %~dp0
-make MMC=mercury_compile copy
+@make MMC=%MMC% copy
 
 @pushd build
-make MMC=mercury_compile %*
+make MMC=%MMC% %*
 @popd
 
 @pushd docs
-make MMC=mercury_compile %*
+make MMC=%MMC% %*
 @popd
 
 @popd
+@exit /b 0
+
+:SET_HOME
+    @endlocal && ( set %1="%MERCURY_HOME%\bin\mmc" ) && exit /b 0
+
+:FIND_IN_PATH
+    @set RESULT=%~dp$PATH:1
+    @endlocal && ( set %2=%RESULT%mmc ) && exit /b 0
