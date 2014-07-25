f89bdae74223c8f7e181f10ab31ae86b5e685730
Sebastian Godelet <sebastian.godelet+github@gmail.com>
2014-07-25 08:02:57 +0200

    Makefile, build.cmd: updated to upstream, supporting WinXP (again)

--- Makefile
+++ Makefile
@@ -3,43 +3,43 @@ include Project.options
 
 .PHONY: default
 default:
-	cd src && $(MAKE) default
+	cd src && "$(MAKE)" default
 
 .PHONY: install
 install:
-	cd src && $(MAKE) install
+	cd src && "$(MAKE)" install
 
 .PHONY: runtests
 runtests:
-	cd tests && $(MAKE) runtests
+	cd tests && "$(MAKE)" runtests
 
 .PHONY: runtests-verbose
 runtests-verbose:
-	cd tests && $(MAKE) runtests-verbose
+	cd tests && "$(MAKE)" runtests-verbose
 
 .PHONY: clean
 clean:
-	cd src && $(MAKE) clean
-	cd tests && $(MAKE) clean
-	cd tools && $(MAKE) clean
+	cd src && "$(MAKE)" clean
+	cd tests && "$(MAKE)" clean
+	cd tools && "$(MAKE)" clean
 
 .PHONY: realclean
 realclean:
-	cd src && $(MAKE) realclean
-	cd tests && $(MAKE) realclean
+	cd src && "$(MAKE)" realclean
+	cd tests && "$(MAKE)" realclean
 
 .PHONY: tags
 tags:
-	cd src && $(MAKE) tags
-	cd tests && $(MAKE) tags
+	cd src && "$(MAKE)" tags
+	cd tests && "$(MAKE)" tags
 
 .PHONY: doc
 doc:
-	cd doc && $(MAKE) default
+	cd doc && "$(MAKE)" default
 
 .PHONY: tools
 tools:
-	cd tools && $(MAKE) default
+	cd tools && "$(MAKE)" default
 
 .PHONY: all
 all: tools default doc runtests-verbose
--- build.cmd
+++ build.cmd
@@ -10,10 +10,18 @@
 @if %oldCP% NEQ %newCP% chcp %newCP% 1>nul
 
 @if defined MERCURY_HOME (
-   call :SET_HOME MERCURY_COMPILER
+    call :SET_HOME MMC mmc
+    if not exist "!MMC!" (
+        call :SET_HOME MMC mercury_compile
+    )
 ) else (
-   call :FIND_IN_PATH mercury.bat MERCURY_COMPILER MERCURY_HOME
+    call :FIND_IN_PATH mercury.bat MERCURY_COMPILER MERCURY_HOME mmc
+    if not exist "!MMC!" (
+        call :FIND_IN_PATH mercury.bat MERCURY_COMPILER MERCURY_HOME mercury_compile
+    )
 )
+
+@if not defined MMC @set MMC=%MERCURY_COMPILER%
 
 @if not exist "Makefile" (
     if exist "%cd%\src\Makefile" (
@@ -23,7 +31,7 @@
     )
 )
 
-@if defined MERCURY_COMPILER goto :MAKE
+@if defined MMC goto :MAKE
 @echo Cannot find Mercury compiler executable, MERCURY_HOME=%MERCURY_HOME%
 @rem ember the previous codepage (very important for Windows XP)
 @if %oldCP% NEQ %newCP% chcp %oldCP% 1>nul
@@ -32,7 +40,7 @@
 :MAKE
     @if defined SRC_SUBDIR @pushd "%SRC_SUBDIR%"
         @set MERCURY_CONFIG_DIR="%MERCURY_HOME%\lib\mercury"
-        @call gmake MMC="%MERCURY_COMPILER:\=/%" MERCURY_HOME="%MERCURY_HOME:\=/%" %*
+        @call gmake MMC="%MMC:\=/%" MERCURY_HOME="%MERCURY_HOME:\=/%" %*
         @set MAKE_RESULT=%ERRORLEVEL%
     @if defined SRC_SUBDIR @popd
     @rem ember the previous codepage (very important for Windows XP)
@@ -41,12 +49,14 @@
 
 :SET_HOME
     @setlocal enabledelayedexpansion
-    @endlocal && (set %1="%MERCURY_HOME%\bin\mercury_compile") && exit /b 0
+    @set BIN=%~2
+    @endlocal && (set %1="%MERCURY_HOME%\bin\%BIN%") && exit /b 0
 
 :FIND_IN_PATH
     @setlocal enabledelayedexpansion enableextensions
     @set RESULT=%~dp$PATH:1
+    @set BIN=%~4
     @set STRIP=%RESULT%~~~
     @set HOME=%STRIP:bin~~~=%
     @set HOME=%HOME:bin\~~~=%
-    @endlocal && (set %2=%RESULT%mercury_compile) && (set %3=%HOME%) && exit /b 0
+    @endlocal && (set %2=%RESULT%%BIN%) && (set %3=%HOME%) && exit /b 0
